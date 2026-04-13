e08c3847d798c9833e9cefb29a49011383d2dbb2
Bhautik <bhautikrchudasama@gmail.com>
2026-04-13 12:08:32 +0530

    chore: fix lint

--- internal/git/remote.go
+++ internal/git/remote.go
@@ -2,6 +2,7 @@
 package git
 
 import (
+	"context"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -16,7 +17,7 @@ var repoFullNamePattern = regexp.MustCompile(`github\.com[:/]([^/]+/[^/.\s]+?)(?
 // GetRemoteFullName returns the "owner/repo" for the git remote origin in the
 // given directory, or empty string if it cannot be determined.
 func GetRemoteFullName(dir string) string {
-	cmd := exec.Command("git", "-C", dir, "remote", "get-url", "origin") // #nosec G204 -- dir comes from os.Getwd()
+	cmd := exec.CommandContext(context.Background(), "git", "-C", dir, "remote", "get-url", "origin") // #nosec G204 -- dir comes from os.Getwd()
 	out, err := cmd.Output()
 	if err != nil {
 		return ""
